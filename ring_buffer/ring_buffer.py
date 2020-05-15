cd17e0b945ec701f5c473e8cfdb7e3916d950125
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-15 02:21:40 -0400

    1/2 of tests pass for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,29 @@
+from singly_linked_list import LinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = LinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_end(item)
+            self.current = self.storage.tail
+        else:
+            if self.current == self.storage.tail:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
+            else:
+                self.storage.insert_after(self.current, item)
+                self.current = self.current.next_node
 
     def get(self):
-        pass
+        list_buffer_contents = []
+        node = self.storage.head
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next_node
+        return list_buffer_contents
